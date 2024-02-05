295f29d91647a0ac34444785c1d3058a0735e1e7
Gaurav <gaurav.ssnl@gmail.com>
2024-02-06 04:13:47 +0530

    Add  Constants file.

---
+++ app/src/main/java/com/gauravssnl/bypassrootcheck/pro/Constants.kt
@@ -0,0 +1,58 @@
+package com.gauravssnl.bypassrootcheck.pro
+
+val knownRootAppsPackages = arrayOf(
+    "com.noshufou.android.su",
+    "com.noshufou.android.su.elite",
+    "eu.chainfire.supersu",
+    "com.koushikdutta.superuser",
+    "com.thirdparty.superuser",
+    "com.yellowes.su",
+    "com.topjohnwu.magisk",
+    "com.kingroot.kinguser",
+    "com.kingo.root",
+    "com.smedialink.oneclickroot",
+    "com.zhiqupk.root.global",
+    "com.alephzain.framaroot"
+)
+
+val knownDangerousAppsPackages = arrayOf(
+    "com.koushikdutta.rommanager",
+    "com.koushikdutta.rommanager.license",
+    "com.dimonvideo.luckypatcher",
+    "com.chelpus.lackypatch",
+    "com.ramdroid.appquarantine",
+    "com.ramdroid.appquarantinepro",
+    "com.android.vending.billing.InAppBillingService.COIN",
+    "com.android.vending.billing.InAppBillingService.LUCK",
+    "com.chelpus.luckypatcher",
+    "com.blackmartalpha",
+    "org.blackmart.market",
+    "com.allinone.free",
+    "com.repodroid.app",
+    "org.creeplays.hack",
+    "com.baseappfull.fwd",
+    "com.zmapp",
+    "com.dv.marketmod.installer",
+    "org.mobilism.android",
+    "com.android.wp.net.log",
+    "com.android.camera.update",
+    "cc.madkite.freedom",
+    "com.solohsu.android.edxp.manager",
+    "org.meowcat.edxposed.manager",
+    "com.xmodgame",
+    "com.cih.game_cih",
+    "com.charles.lpoqasert",
+    "catch_.me_.if_.you_.can_"
+)
+
+val knownRootCloakingPackages = arrayOf(
+    "com.devadvance.rootcloak",
+    "com.devadvance.rootcloakplus",
+    "de.robv.android.xposed.installer",
+    "com.saurik.substrate",
+    "com.zachspong.temprootremovejb",
+    "com.amphoras.hidemyroot",
+    "com.amphoras.hidemyrootadfree",
+    "com.formyhm.hiderootPremium",
+    "com.formyhm.hideroot"
+)
