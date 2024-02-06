aff5683cb65d6146b80a00d58d42d8bdc72d5ff4
Gaurav <gaurav.ssnl@gmail.com>
2024-02-07 01:02:54 +0530

    Add Native System  C/C++ Hooks to bypass root detection. Make root detection bypass  efficent &  independent of any root detection library .

--- app/src/main/cpp/helper.h
+++ app/src/main/cpp/helper.h
@@ -7,12 +7,13 @@
 #include <string>
 #include <jni.h>
 
-
-jclass (*original_FindClass)(JNIEnv *env, const char *name);
-
 FILE *(*original_fopen)(const char *filename, const char *mode);
 
 FILE *fake_fopen(const char *filename, const char *mode);
+
+int (*original_stat)(const char *, struct stat *);
+
+int fake_stat(const char *, struct stat *);
 
 jint (*RootBeerNative_setLogDebugMessages)(JNIEnv *env, jobject thiz, jboolean debug);
 
@@ -25,3 +26,5 @@ jint RootBeerNative_checkForMagiskUDS_Fake(JNIEnv *env, jobject thiz);
 jint *RootBeerNative_checkForRoot_Fake(JNIEnv *env, jobject thiz, jobjectArray pathsArray);
 
 jint RootBeerNative_setLogDebugMessages_Fake(JNIEnv *env, jobject thiz, jboolean);
+
+bool is_file_related_to_root(const char *filename);
--- app/src/main/cpp/nativehooks.cpp
+++ app/src/main/cpp/nativehooks.cpp
@@ -3,13 +3,14 @@
 #include "logger.h"
 
 #include <dlfcn.h>
+#include <sys/stat.h>
 
 static HookFunType hook_func = nullptr;
 
 void on_library_loaded(const char *name, void *handle) {
     if (strstr(name, "libtool-checker.so") || strstr(name, "libtoolChecker.so")) {
         LOGI("Trying to hook libtool Checker library :: %s", name);
-        // Root Beer Fresh hooks
+        // Root Beer Fresh & Root Beer hooks
         void *target1 = dlsym(handle,"Java_com_kimchangyoun_rootbeerFresh_RootBeerNative_checkForMagiskUDS");
         if (target1)
             hook_func(target1, (void *) RootBeerNative_checkForMagiskUDS_Fake,
@@ -31,8 +32,7 @@ void on_library_loaded(const char *name, void *handle) {
         if (target5)
             hook_func(target1, (void *) RootBeerNative_setLogDebugMessages_Fake,
                       (void **) &RootBeerNative_setLogDebugMessages);
-
-        LOGI("All native hooks for root bypass done. Enjoy :)");
+        LOGI("All native hooks applied for RootBeer / RootBeer Fresh :)");
     }
 }
 
@@ -40,23 +40,47 @@ void on_library_loaded(const char *name, void *handle) {
 extern "C" [[gnu::visibility("default")]] [[gnu::used]]
 NativeOnModuleLoaded native_init(const NativeAPIEntries *entries) {
     hook_func = entries->hook_func;
+    // System Hooks
+    hook_func((void *) fopen, (void *) fake_fopen, (void **) &original_fopen);
+    hook_func((void *) stat, (void *) fake_stat, (void **) &original_stat);
+    LOGI("All System hooks applied for bypassing root check.)");
     return on_library_loaded;
 }
 
+FILE *fake_fopen(const char *filename, const char *mode) {
+    LOGD("Inside fake_fopen. Filename :: %s", filename);
+    if (is_file_related_to_root(filename)) return nullptr;
+    return original_fopen(filename, mode);
+}
+
+int fake_stat(const char *filename, struct stat *file_info) {
+    LOGD("Inside fake_stat. Filename :: %s ", filename);
+    if (is_file_related_to_root(filename)) return -1;
+    return original_stat(filename, file_info);
+}
+
+bool is_file_related_to_root(const char *filename) {
+    if (strstr(filename, "su") || strstr(filename, "/proc/net/unix") ||
+        strstr(filename, "magisk") || strstr(filename, "busybox") ||
+        strstr(filename, "/data/adb/.boot_count"))
+        return true;
+    return false;
+}
+
 jint RootBeerNative_checkForMagiskUDS_Fake(JNIEnv *env, jobject thiz) {
-    LOGI("Inside checkForMagiskUDS_Fake");
+    LOGD("Inside checkForMagiskUDS_Fake");
     return 0;
 }
 
 jint *RootBeerNative_checkForRoot_Fake(JNIEnv *env, jobject thiz, jobjectArray pathsArray) {
-    LOGI("Inside RootBeerNative_checkForRoot_Fake");
+    LOGD("Inside RootBeerNative_checkForRoot_Fake");
     return 0;
 }
 
 jint RootBeerNative_setLogDebugMessages_Fake(JNIEnv *env, jobject thiz, jboolean) {
-    LOGI("Inside RootBeerNative_setLogDebugMessages_Fake");
+    LOGD("Inside RootBeerNative_setLogDebugMessages_Fake");
     // we want to enable DEBUG flag to see our hooks
     auto res = RootBeerNative_setLogDebugMessages(env, thiz, 1);
-    LOGI("Debug flag set for RootBeerNative_setLogDebugMessages");
+    LOGD("Debug flag set for RootBeerNative_setLogDebugMessages");
     return res;
 }
